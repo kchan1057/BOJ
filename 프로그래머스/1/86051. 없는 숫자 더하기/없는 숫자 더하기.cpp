9e6771f2bce02f829ca04f4fce9c7c1bdd9fa974
sg.chan <163969084+kchan1057@users.noreply.github.com>
2025-05-07 14:54:32 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include <vector>
+#include <numeric>
+using namespace std;
+
+int solution(vector<int> numbers) {
+    int sum = accumulate(numbers.begin(), numbers.end(), 0);
+    return 45-sum;
+}
